ee89f9b10c431cc38351632cf163eba2eda67c00
Chris Opperwall <chriso@ifixit.com>
2015-02-08 16:16:05 -0800

    Add Reddit class for Reddit API operations.
    This class servers as a wrapper for the Reddit API. At the moment, it only has
    methods for getting comments and submissions (posts) from an array of
    usernames. Some other functionality this could hold includes sending an
    authentication request for a reddit user, posting a comment reply on behalf of
    a user, "saving" a post on behalf of a user, etc.

---
+++ Reddit.php
@@ -0,0 +1,52 @@
+<?php
+
+define('BASE_URL', 'http://reddit.com');
+
+/**
+ * Wrapper class for reddit's API
+ */
+class Reddit {
+   
+   /**
+    * Grab the most recent $limit comments from $users.
+    *
+    * @param $users array - An array of usernames.
+    * @param $limit (optional) - The number of comments to grab.
+    */
+   public static function getComments(array $users, $limit = 5) {
+      return self::getItems($users, $limit, 'comments');
+   }
+
+   /**
+    * Grab the most recent $limit submissions from $users.
+    *
+    * @param $users array - An array of usernames.
+    * @param $limit (optional) - The number of submissions (posts) to grab.
+    */
+   public static function getSubmissions(array $users, $limit = 5) {
+      return self::getItems($users, $limit, 'submitted');
+   }
+
+   private static function getItems(array $users, $limit, $type) {
+      $items = [];
+
+      // Get content, parse into JSON, and add all chilren to items array
+      foreach ($users as $user) {
+         $apiResult = file_get_contents(BASE_URL . "/user/$user/$type.json?limit=$limit");
+         $JSONresult = json_decode($apiResult, /* assoc */ true);
+         $children = $JSONresult['data']['children'];
+
+         foreach ($children as $child) {
+            array_push($items, $child['data']);
+         }
+      }
+
+      usort($items, "self::sortByTime");
+      return $items;
+   }
+
+   // Sort items in chronoogical order
+   private static function sortByTime($a, $b) {
+      return $b['created_utc'] - $a['created_utc'];
+   }
+}
