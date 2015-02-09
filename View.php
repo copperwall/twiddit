e2e3622821097004d2bba926d92aa732aab3eddf
Chris Opperwall <chriso@ifixit.com>
2015-02-08 16:13:44 -0800

    Add View class and templates/ directory
    The View class allows you to create a "View" from a specific template file and
    bind variables to the page. This could be helpful for anytime we want to alter
    some HTML before we send it to the client. I left a sample template called
    base.phtml in the templates directory.

---
+++ View.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * View object
+ *
+ * This allows you to create a view from a template file and add variables to 
+ * use in the template.
+ */
+class View {
+   protected $template_dir = './templates/';
+   protected $template_file;
+   protected $vars = [];
+
+   public function __construct($template_file) {
+      $this->template_file = $template_file;
+   }
+
+   /**
+    * Render the template into HTML.
+    */
+   public function render() {
+      if (file_exists($this->template_dir . $this->template_file)) {
+         extract($this->vars);
+         include $this->template_dir . $this->template_file;
+      } else {
+         throw new Exception("{$this->template_dir}{$this->template_file} template not found");
+      }
+   }
+
+   /**
+    * Add a variable to be accessible in the template, by the given name.
+    *
+    * @param $name string - The name to access the variable in the template.
+    * @param $value - The variable to add to the template.
+    */
+   public function addPageVariable($name, $value) {
+      $this->vars[$name] = $value;
+   }
+}
