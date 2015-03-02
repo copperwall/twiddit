cc2d4ee516865995fb484bd8282b654d91d1af3b
Chris Opperwall <chriso@ifixit.com>
2015-03-02 21:04:24 +0000

    Fix routing for static files
    When our page was trying to grab our own css and js files, the request would be
    picked up by our PHP controller and the originally requested file would not be
    returned.
    
    This adds a check in `app.js` that makes sure the `REQUEST_URI` does not
    contain 'public'. If it does, it bypasses our PHP routing and serves the static
    file that is requested from `public/`.
    
    If you add any new css or js files, please place them in either
    `public/js/yourfile.js` or `public/css/yourfile.css`.

--- app.php
+++ app.php
@@ -1,11 +1,26 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
+require 'connect.inc.php';
 require 'vendor/autoload.php';
 require_once('Reddit.php');
 require_once('View.php');
 
+// If request is for the public directory, serve static file (for js/css)
+if (stristr($_SERVER['REQUEST_URI'], 'public')) {
+   return false;
+}
+
 $app = new \Slim\Slim();
+
+$app->get('/', function() use ($app) {
+   include ('headerBar.html');
+});
+
+$app->get('/signin', function()  use ($app) {
+   include 'signin.html';
+});
+
 $app->get('/feed', function() use ($app) {
    $users = ['kn0thing', 'zolokar', 'xiongchiamiov'];
    $comments = Reddit::getComments($users);
@@ -18,9 +33,6 @@ $app->get('/subreddits', function() use ($app) {
 
    echo json_encode($data);
 });
-$app->post('/signin', function()  use ($app) {
-   echo $app->request->post();
-});
 
 $app->get('/reddit_callback', function() use ($app) {
    $req = $app->request();
@@ -28,4 +40,20 @@ $app->get('/reddit_callback', function() use ($app) {
    echo $req->get('state') . "\n";
    echo $req->get('code');
 });
+
+$app->post('/login', function() use ($app, $db) {
+   $username = $app->request->post('username');
+   $password = $app->request->post('password');
+
+   $query = "SELECT * FROM  users where userName='$username' and userPassword='$password'";
+
+    $result = $db->query($query);
+
+    if (empty($query)) {
+        echo 'FAILURE';
+    } else {
+       $app->redirect('/');
+    }
+});
+
 $app->run();
